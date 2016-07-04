f28dee97f483ed2e67f06af4a44f439c1fe297ad
Callum Mann <cm13558@my.bristol.ac.uk>
2016-07-03 19:49:57 -0700

    add gitignore and fix find_point error

--- mathematics/fundamentals/find_point.py
+++ mathematics/fundamentals/find_point.py
@@ -4,5 +4,6 @@ def find_point(px, py, qx, qy):
    return (2*qx - px, 2*qy - py)
 
 if __name__ == "__main__":
-    for line in sys.stdin:
-        print(find_point(*map(int, line.split())))
+    n = int(input())
+    for _ in range(n):
+        print(" ".join(map(str, find_point(*map(int, input().split())))))
